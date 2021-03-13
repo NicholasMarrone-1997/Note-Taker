e6fefbe7196a7103da48656a18faa7d1488b6b48
Nicholas Marrone <nmarrone1997@gmail.com>
2021-03-13 13:59:42 -0500

    Trying to figure out express routing

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
-// DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
-
-const express = require('express');
-
-// EXPRESS CONFIGURATION
+// Dependencies
 // This sets up the basic properties for our express server
-
+const express = require('express');
 // Tells node that we are creating an "express" server
 const app = express();
-
 // need path for filename paths
 const path = require("path");
-
 // need fs to read and write to files
 const fs = require("fs");
 
@@ -27,20 +20,3 @@ app.use(express.static("public"));
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 
-api.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/db/db.json"))
-});
-
-app.post('/api/notes', (req, res) => {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    // const newCharacter = req.body;
-  
-    // // Using a RegEx Pattern to remove spaces from newCharacter
-    // // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    // newCharacter.routeName = newCharacter.name.replace(/\s+/g, '').toLowerCase();
-    // console.log(newCharacter);
-  
-    // characters.push(newCharacter);
-    // res.json(newCharacter);
-  });
