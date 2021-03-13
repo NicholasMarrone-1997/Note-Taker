b8c4e36cce0005393b0af6415c98a383e50e8a1b
Nicholas Marrone <nmarrone1997@gmail.com>
2021-03-13 14:58:23 -0500

    Pathing issues with db.json

--- server.js
+++ server.js
@@ -32,10 +32,31 @@ app.get('/notes', function (req, res) {
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 app.get("/api/notes", function(err, res){
-
+    try{
+        let noteData = fs.readFileSync("\db\db.json");
+        noteData = JSON.parse(noteData);
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 app.post("/api/notes", function(err, res){
 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// Start app on specified port
+app.listen(PORT, function() {
+    console.log("Listening on: " + PORT);
 });
