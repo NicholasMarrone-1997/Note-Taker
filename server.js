66eaf83099a10b18f4d4ccc4a02247be330ee0c3
Nicholas Marrone <nmarrone1997@gmail.com>
2021-03-13 14:43:23 -0500

    Built out shells of the necessary fet and post functions, 2 of 4 complete

--- server.js
+++ server.js
@@ -20,7 +20,22 @@ app.use(express.static("public"));
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 
-// Respond with notes.html file when a get request is made to the notes page
-app.get('/public/notes', function (req, res) {
+// GET /notes should return the notes.html file.
+app.get('/notes', function (req, res) {
     res.sendFile(path.join(__dirname + '/notes.html'));
   });
+
+// GET * should return the index.html file.
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+  });
+
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+app.get("/api/notes", function(err, res){
+
+});
+
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+app.post("/api/notes", function(err, res){
+
+});
