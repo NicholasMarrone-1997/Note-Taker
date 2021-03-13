4915c37a4911f202c55fd608ebb49367d08a5d04
Nicholas Marrone <nmarrone1997@gmail.com>
2021-03-13 16:54:44 -0500

    Trying to get save button to function

--- server.js
+++ server.js
@@ -7,12 +7,18 @@ const app = express();
 const path = require("path");
 // need fs to read and write to files
 const fs = require("fs");
+// need uuid to generate unique id's for each saved note
+const {
+    v4: uuidv4
+} = require('uuid');
 
 // Sets an initial port. We"ll use this later in our listener
 const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 app.use(express.static("public"));
 
@@ -22,41 +28,41 @@ app.use(express.static("public"));
 
 // GET /notes should return the notes.html file.
 app.get('/notes', function (req, res) {
-    res.sendFile(path.join(__dirname + '/notes.html'));
-  });
+    res.sendFile(path.join(__dirname + '/public/notes.html'));
+});
 
 // GET * should return the index.html file.
-  app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-  });
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
-app.get("/api/notes", function(err, res){
-    try{
-        let noteData = fs.readFileSync("db\db.json");
+app.get("/api/notes", function (req, res) {
+    try {
+        let noteData = fs.readFileSync("db/db.json");
         noteData = JSON.parse(noteData);
+        
     } catch (err) {
         console.log(err);
     }
 });
 
-// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-app.post("/api/notes", function(err, res){
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. 
+// You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+app.post("/api/notes", function (req, res) {
+    try {
+        let noteData = fs.readFileSync("db/db.json");
+        noteData = JSON.parse(noteData);
 
+
+    } catch (err) {
+        console.log(err);
+    }
 });
-
-
-
-
-
-
-
-
-
-
 
 
 // Start app on specified port
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("Listening on: " + PORT);
 });
+
